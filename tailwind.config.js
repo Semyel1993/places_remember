58b63ebda96888543c0810bcf7861ba99cf4af80
Semyel1993 <sementip@gmail.com>
2024-05-07 19:25:04 +0700

    Add home page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+      content: [
+          './places/templates/**/*.html'
+      ],
+      theme: {
+          borderRadius: {
+              'lg': '2.0rem',
+          },
+          extend: {
+              boxShadow: {
+                  'xl': '0 0 15px 0 rgba(0, 0, 0, 0.3)',
+              }
+          }
+      },
+      plugins: [],
+}
